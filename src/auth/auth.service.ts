d4c56013ea298e219ec4ea10b0482d6e5c9f6c70
anuj66 <saraogi.anuj9@gmail.com>
2022-08-18 21:58:59 +0530

    Added Login and Signup functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,64 @@
-import {Injectable, Req} from "@nestjs/common";
-import {PrismaService} from "../prisma/prisma.service";
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
 
-    constructor(private prisma: PrismaService) {
-    }
+  async login(dto: AuthDto) {
 
-    login() {
-        return {msg: 'I have logged in'}
-    }
+    // Find user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email
+      }
+    })
 
-    signup() {
-        return {msg: 'I have signed up'}
+    //If user not found throw exception
+    if(!user) throw new ForbiddenException("Credentials Incorrect")
+
+    // Check if password is correct
+    const passwordMatch = await argon.verify(user.hash, dto.password)
+
+    // If incorrect password throw exception
+    if(!passwordMatch) throw new ForbiddenException("Credentials Incorrect")
+
+    delete user.hash
+    return { msg: 'Successfully Logged In', user };
+  }
+
+  async signup(dto: AuthDto) {
+    try {
+      // Generate the password hash
+      const hash = await argon.hash(dto.password);
+
+      //Save the new user in db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+
+      return {
+        msg: 'User signed up successfully',
+        user,
+      };
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('User with this email already registered');
+        }
+      }
+      throw error;
     }
+  }
 }
